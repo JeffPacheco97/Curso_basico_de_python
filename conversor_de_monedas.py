e121e339a11df90a9ba5f6388d01499b79091f9c
Jefferson Pacheco <andres.pacheco.97@gmail.com>
2022-09-27 14:14:55 -0500

    Limpiamos el código incluyendo funciones

--- conversor_de_monedas.py
+++ conversor_de_monedas.py
@@ -3,21 +3,21 @@
 menu = """🪙 ¡Bienvenido a Convertland, tu convertidor de monedas favorito! 🪙
 
 1-Pesos colombianos (COP)
-2-Pesos Mexicanos (MEX)
+2-Pesos Mexicanos (MXN)
 3-Pesos Argentinos (ARS)
 
 Elige la opción que más se adapte a tus necesidades. 😸
 """
 opcion = int(input(menu))
 
-#OPCIONES A ELEGIR EN CONDICIONALES IF
-if opcion == 1:
+#FUNCIÓN DE CONVERTIR MONEDA
+def convertir_moneda(tipo_moneda):
 
-#SOLICITAR VALOR EN COP
-    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+#SOLICITAR VALOR de MONEDA
+    moneda = input("¿Cuántos " + tipo_moneda + " tienes?: ")
 
 #CASTEAR VALORES DE STR A FLOTANTE
-    pesos = float(pesos)
+    moneda = float(moneda)
 
 #SOLICITAR TASA DE CAMBIO
     valor_dolar = input("¿A cuánto equivale el dolar hoy?")
@@ -26,7 +26,7 @@ if opcion == 1:
     valor_dolar = float(valor_dolar)
 
 #REALIZAR CONVERSIÓN
-    dolares = pesos / valor_dolar
+    dolares = moneda / valor_dolar
 
 #REDONDEAR A DOS CON ROUND
     dolares = round(dolares,2)
@@ -35,70 +35,27 @@ if opcion == 1:
     dolares = str(dolares)
 
 #CASTEAR NUEVAMENTE PESOS DE FLOAT A STR PARA IMPRIMIR
-    pesos = str(pesos)
+    moneda = str(moneda)
 
 #IMPRIMIR RESULTADOS
-    print("El valor en dólares de sus $" + pesos + " es: $" + dolares + " dólares")
+    print("El valor en dólares de sus $" + moneda + " es: $" + dolares + " dólares")
 
-    
+
+
+
+#OPCIÓN 1 
+if opcion == 1:
+    convertir_moneda("pesos colombianos (COP)")
+
+#OPCIÓN 2 
 elif opcion == 2:
-#SOLICITAR CANTIDAD DE PESOS MEXICANOS DISPONIBLES
-    mxn = input("¿Cuántos pesos mexicanos tienes?:  ")
+    convertir_moneda("pesos mexicanos (MXN)")
 
-#CASTEAR DE STR A FLOAT
-    mxn = float(mxn)
-
-#SOLICITAR TASA DE CAMBIO
-    valor_dolar = input("¿Cuál es la tasa de cambio para el MXN hoy?")
-
-#CASTEAR DE STR A FLOAT
-    valor_dolar = float(valor_dolar)
-
-#HACER CONVERSIÓN SEGÚN TASA DE CAMBIO
-    dolar = mxn / valor_dolar
-
-#REDONDEAR A DOS CON ROUND
-    dolar = round(dolar, 2)
-
-#CASTEAR DE FLOAT A STR
-    dolar = str(dolar)
-
-#CASTEAR NUEVAMENTE MXN DE FLOAT A STR
-    mxn = str(mxn)
-
-#IMPRIMIR VALORES EN PANTALLA
-    print("Sus $"+ mxn + " pesos mexicanos (MXN) equivalen a: $" + dolar + " dólares")
-
-
+#OPCIÓN 3 
 elif opcion == 3:
-
-#SOLICITAR CANTIDAD DE PESOS ARGENTINOS DISPONIBLES
-    ars = input("¿Cuántos pesos argentinos tienes?:  ")
-
-#CASTEAR DE STR A FLOAT
-    ars = float(ars)
-
-#SOLICITAR TASA DE CAMBIO
-    valor_dolar = input("¿Cuál es la tasa de cambio para el ARS hoy?")
-
-#CASTEAR DE STR A FLOAT
-    valor_dolar = float(valor_dolar)
-
-#HACER CONVERSIÓN SEGÚN TASA DE CAMBIO
-    dolar = ars / valor_dolar
-
-#REDONDEAR A DOS CON ROUND
-    dolar = round(dolar, 2)
-
-#CASTEAR DE FLOAT A STR
-    dolar = str(dolar)
-
-#CASTEAR NUEVAMENTE MXN DE FLOAT A STR
-    ars = str(ars)
-
-#IMPRIMIR VALORES EN PANTALLA
-    print("Sus $"+ ars + " pesos argentinos (ARS) equivalen a: $" + dolar + " dólares")
-    
+    convertir_moneda("pesos argentinos (ARS)") 
+      
+#OPCIÓN DEFAULT
 else:
     print("Ingresa un valor válido, no seas así. 😿")
 
