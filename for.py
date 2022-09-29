f1114a6093ab7c76d750e6693367cc1c45d33a94
Jefferson Pacheco <andres.pacheco.97@gmail.com>
2022-09-29 13:36:42 -0500

    Cree dos mini programas: uno que detecta si un numero es primo; el otro es un minijuego

--- for.py
+++ for.py
@@ -1,8 +1,13 @@
-#SOLICITAMOS NUMERO A MULTIPLICAR
-numero = int(input("¿De cuál número desea obtener la tabla de multiplicar?: "))
-#INICIAMOS CICLO FOR EN RANGO DE 1 A 11 PARA TENER NUMEROS DE 0 A A 10
-for i in range(1,11):
-#MULTIPLICAMOS EL NUMERO DE USUARIO POR EL CONTADOR
-    multiplicacion = numero * i
-#IMPRIMIMOS EL RESULTADO EN UNA TABLA
-    print("El número ingresado es: " + str(numero) + " y su tabla es: " + str(multiplicacion))
+# #SOLICITAMOS NUMERO A MULTIPLICAR
+# numero = int(input("¿De cuál número desea obtener la tabla de multiplicar?: "))
+# #INICIAMOS CICLO FOR EN RANGO DE 1 A 11 PARA TENER NUMEROS DE 0 A A 10
+# for i in range(1,11):
+# #MULTIPLICAMOS EL NUMERO DE USUARIO POR EL CONTADOR
+#     multiplicacion = numero * i
+# #IMPRIMIMOS EL RESULTADO EN UNA TABLA
+#     print("El número ingresado es: " + str(numero) + " y su tabla es: " + str(multiplicacion))
+
+for letra in "Python":
+    if letra == "h":
+        continue
+    print ("Letra actual : " + letra)
